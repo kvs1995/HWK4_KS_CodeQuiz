c126b395199e740812680952d7ce4bec6c45eb9c
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2021-10-22 02:03:36 -0500

    added function to create EndScreen when the timer runs out

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,15 @@ var timerEl = document.getElementById('countdown');
 
 timerEl.textContent = "Play To Start Timer!"
 
+//need function to display end screen after quiz is over
+var endScreen = document.getElementsByClassName('info-description');
+// var endScreenMessage = "ooga booga"
+
+
+
 playBtn.addEventListener("click",function() {
     //swtich the content in the card
-    
+
     //there is a replace Child funciton in javascript -- read about ti
 
     //remove the Code Quiz title, descripiton, and play button
@@ -29,10 +35,19 @@ playBtn.addEventListener("click",function() {
             timerEl.textContent = "Time's Up!"
 
             //need to call a function that displays the finishing score and the play again/view highscore button
+            function endScreen() {
 
+                var timesUpMessage = document.createElement("p");
+                timesUpMessage.textContent = "Ooga Booga"
+                document.body.children[1].children[0].setAttribute("style","text-align:center")
+                document.body.children[1].children[0].appendChild(timesUpMessage)
+                
+            }
+            endScreen();
         
         }
         timeLeft--;
     },100);
 })
 
+//need function for when a button is selected for it to go to the next one. need a for loop
